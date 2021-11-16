44cc708c2b8b78c46fd048a34442b3f55b39db1d
ruslanlupashtyan <ruslanl1488@gmail.com>
2021-11-16 23:05:06 +0200

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,87 @@
+$(document).ready (function() {
+  $('.news__slider').slick({
+    dots: false,
+    infinite: false,
+    arrows: true,
+    prevArrow: '.news__slider_arrow.prev',
+    nextArrow: '.news__slider_arrow.next',          
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false
+
+        }
+      }
+    ]
+  });
+
+  $("button.news__slider_arrow").click(function(){
+    let $activeArray = $(".slick-active");
+    if( $(this).hasClass("next") ){
+      // net pos
+      let $next = $($activeArray[$activeArray.length - 1]).next();
+      if( $next.length === 0 ){
+        $('button.news__slider_arrow.next').addClass("disabled");
+        $('button.news__slider_arrow.prev').removeClass("disabled");
+      }
+      console.log($next)
+    } else {
+      // prev pos
+      let $prev = $($activeArray[0]).prev();
+      if( $prev.length === 0 ){
+        $('button.news__slider_arrow.next').removeClass("disabled");
+        $('button.news__slider_arrow.prev').addClass("disabled");
+      }
+    }
+  });
+
+});
+
+let hamburger = document.getElementById('hamburger')
+let menu = document.getElementById('menu')
+let sMenu = document.getElementById('s-menu')
+
+hamburger.onclick = function(){
+  menu.classList.toggle("active");
+  sMenu.classList.toggle('active');
+  menu.appendChild(sMenu);
+  console.log(menu)
+}
+
+// $('div.lang__item').click(function(){
+//   let $sneeky_ul = $('ul.lang__list');
+
+//   $sneeky_ul.fadeToggle();
+// });
+
+// $('ul.lang__list > li').click(function(){
+//   let $sneeky_ul = $('ul.lang__list'),
+//     $self = $(this);
+
+//   $('div.lang__item').html($self.html());
+
+//   $sneeky_ul.fadeToggle();
+// });
